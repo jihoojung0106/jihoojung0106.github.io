1a5c3d241c309061a6f4dd2e82dddb42fdc2b8e3
jihoojung0106 <jjh123579@snu.ac.kr>
2026-05-07 12:50:41 +0000

    Deploying to gh-pages from @ jihoojung0106/jihoojung0106.github.io@2f1e007e40c58577a7e7d9dbcb248d36c0cb4ec2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -433,7 +433,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75%72.%65%6D%61%69%6C@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%6A%69%68%6F%6F%6A%75%6E%67@%6B%61%69%73%74.%61%63.%6B%72", "_blank");
         },
       },{
         id: 'social-github',
@@ -441,5 +441,19 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://github.com/jihoojung0106", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/jihoo-jung-b77159210", "_blank");
+        },
+      },{
+        id: 'social-scholar',
+        title: 'Google Scholar',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://scholar.google.com/citations?user=Vawl0tQAAAAJ", "_blank");
         },
       },];
